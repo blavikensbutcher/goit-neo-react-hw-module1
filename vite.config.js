d40b06dbe76f4244d4a269cc58bd7b6c2e31cc27
blavikensbutcher <v.pidnebesnyi@omisoft.net>
2025-11-29 14:24:43 +0200

    upd - vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/goit-neo-react-hw-module1/',
   plugins: [react()],
   build: {
-    sourcemap: true
+    sourcemap: true,
   },
-})
+});
